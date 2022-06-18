73446a81ff4ad3e7130cb663103aaa3c8ab9b6de
lameesnd <lamees.naseraldean@gmail.com>
2022-06-18 20:26:48 +0300

    basic templates

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'pages/home.dart';
+import 'package:world_time/pages/home.dart';
 
 void main() {
   runApp(MaterialApp(
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -11,7 +11,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Home Screen'),
+      body: SafeArea(child: Text('Home Screen')),
     );
   }
 }
