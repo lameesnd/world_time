73b4492f5310343d300a80c160647c1e1e52bbf9
lameesnd <lamees.naseraldean@gmail.com>
2022-06-19 21:11:55 +0300

    usage of http package

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -8,27 +8,6 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
-  void getData() async {
-    //simulate network request for a username
-    String username = await Future.delayed(Duration(seconds: 3), () {
-      return 'lamees';
-    });
-    //simulate network request to get bio for username
-    String bio = await Future.delayed(Duration(seconds: 2), () {
-      return 'programmer, animal lover & adventurous';
-    });
-
-    // statement won't run before the result is retrieved
-    print('$username - $bio');
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getData();
-    print('hi');
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
